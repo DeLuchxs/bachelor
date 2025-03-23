eddbd10b391706557920245818fd63fc7a55ba74
DeLuchxs <liberi1418@gmail.com>
2025-03-23 11:28:27 +0100

    checksum test

--- code/checksumtest.py
+++ code/checksumtest.py
@@ -1,6 +1,25 @@
+import cantools
+
+
 canFrameID = 0x5b3
-throttleRInput = 0xDEADBEEF
 messageCounter = 0x00
+
+db = cantools.database.load_file('dbc/j1939_1.dbc', strict=False)
+gasLeverMessage = db.get_message_by_name('TSC1')
+
+throttleRInput = gasLeverMessage.encode({ 
+        'EngOverrideCtrlMode': 3, #3: Speed / Torque Limit Control Mode
+        'EngRequestedSpeedCtrlConditions': 0, # 0: Transient Optimized for driveline disengaged and non-lockup conditions
+        'OverrideCtrlModePriority': 0, # 0: Highest Priority
+        'EngRequestedSpeed_SpeedLimit': 1027.25,
+        'EngRequestedTorque_TorqueLimit': -123,
+        'TSC1TransRate': 7, # Transmission Rate of 100ms
+        'TSC1CtrlPurpose': 31, # Temporary PowerTrain Control
+        'EngRequestedTorqueHighResolution': 1.875,
+        'MessageCounter': 15,
+        'MessageChecksum': 0
+    })
+
 
 checksum = 0x00
 data_bytes = throttleRInput[:7]
